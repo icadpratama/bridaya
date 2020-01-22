12123f43395feb763f70078e97b267c49cbf0153
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2020-01-22 13:49:56 +0700

    Add GCP dependency

--- src/main/java/team/kasta/bridaya/model/information/Product.java
+++ src/main/java/team/kasta/bridaya/model/information/Product.java
@@ -5,10 +5,73 @@ import team.kasta.bridaya.model.audit.UserDateAudit;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "producst")
+@Table(name = "products")
 public class Product extends UserDateAudit {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "kind")
+    private String kind;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "detail")
+    private String detail;
+
+    public Product() {
+    }
+
+    public Product(Long id, String type, String kind, String name, String detail) {
+        this.id = id;
+        this.type = type;
+        this.kind = kind;
+        this.name = name;
+        this.detail = detail;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
 }
